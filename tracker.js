70278d8bb3e24b65f92339c1b2c926239901f1a1
GeekHornsFan <Behnke19@gmail.com>
2021-10-26 19:41:28 -0500

    Add url params for setting colors
    New options for setting the colors of the progress bar and text.

--- tracker.js
+++ tracker.js
@@ -1,18 +1,35 @@
-let pathParam = window.location.search.substring(1);
-let myId = '';
-let teamId = '';
-let param = pathParam.split("=");
+const urlParams = new URLSearchParams(window.location.search);
+let root = document.documentElement;
+let myId = urlParams.get("participantID");
+let teamId = urlParams.get("teamID");
 let url = '';
-if (param[0] == "participantID") {
-  myId = param[1];
+// Logic for individual vs team data.
+if (myId) {
   let myUrl = 'https://www.extra-life.org/api/1.3/participants/' + myId;
   url = myUrl;
   document.getElementById("label-team").hidden=true;
-} else if (param[0] == "teamID") {
-  teamId = param[1];
+} else if (teamId) {
   let teamUrl = 'https://www.extra-life.org/api/1.3/teams/' + teamId;
   url = teamUrl;
   document.getElementById("label-mine").hidden=true;
+}
+
+// logic to influence the style of the page.
+let progressBackgroundColor = urlParams.get("progressBackgroundColor");
+if (progressBackgroundColor) {
+  root.style.setProperty("--progress-color-background", "#" + progressBackgroundColor);
+}
+let progressColor = urlParams.get("progressColor");
+if (progressColor) {
+  root.style.setProperty("--progress-color", "#" + progressColor);
+}
+let labelColor = urlParams.get("labelColor");
+if (labelColor) {
+  root.style.setProperty("--label-color", "#" + labelColor);
+}
+let percentTextColor = urlParams.get("percentTextColor");
+if (percentTextColor) {
+  document.getElementById("progressBar").style.color = "#" + percentTextColor;
 }
 
 function reload() {
